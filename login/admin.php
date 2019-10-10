2e9da5d22b9a78eb69802632256b0b91532f81a2
ariefhidayatullah <55170593+ariefhidayatullah@users.noreply.github.com>
2019-10-10 20:57:45 +0700

    update minggu ke-5

---
+++ login/admin.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>admin</title>
+</head>
+<body>
+	<h1>Selamat datang admin</h1>
+	<a href="index.php">logout</a>
+</body>
+</html>
