fef2d77a6392930c019380fe98f6db015e573205
Fabryzal Adam Pramudya <ryzaldm@gmail.com>
2019-10-16 08:32:45 +0700

    test

--- ZaLL/form/index.php
+++
@@ -1,8 +0,0 @@
-<!-- Penanganan form dengan method GET -->
-<form method="get" action="tampil.php">
-	<label>Masukkan Nama</label><br/>
-	<input type="text" name="nama"><br/>
-	<label>Masukkan Usia</label><br/>
-	<input type="text" name="usia"><br/>
-	<input type="submit" value="simpan"><br/>
-</form>
