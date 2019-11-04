c1a2c4e1129ce89d41cf12ccc05528453ef8d5cf
Fabryzal Adam Pramudya <ryzaldm@gmail.com>
2019-11-04 08:01:57 +0700

    test

---
+++ CRUD bahan/function.php
@@ -0,0 +1,62 @@
+<?php 
+//untuk koneksi
+$conn = mysqli_connect('localhost', 'root', '', 'printonline');
+
+//membuat function agar jadi satu, supaya jadi efektif dan efisien
+function query($query){
+	//untuk memasukkan variabel $conn karena kalau langsung tidak bisa, grgr scope
+	global $conn;
+	//membuat array kosong untuk menampung data
+	$result = mysqli_query($conn, $query);
+	//untuk mengambil data dari database
+	$rows = [];
+	
+	while ($row = mysqli_fetch_assoc($result)) {
+		$rows[] = $row;
+	}
+	return $rows;
+
+}
+
+function tambah_bahan($data){
+	global $conn;
+	//ambil data dari tiap elemen dalam form
+	$id_bahan = $data['id_bahan'];
+	$nama_bahan = $data['nama_bahan'];
+	$id_produk = $data['id_produk'];
+	$stok = $data['stok'];
+
+	$query = "INSERT INTO bahan VALUES ('$id_bahan', '$nama_bahan', '$id_produk', '$stok')";
+	mysqli_query($conn, $query);
+	return  mysqli_affected_rows($conn);
+}
+
+
+function hapus_bahan($id){
+	global $conn;
+	mysqli_query($conn, "DELETE FROM `bahan` WHERE id= $id");
+	return mysqli_affected_rows($conn);
+}
+
+function ubah_bahan($data){
+	global $conn;
+
+	$id_bahan = $data['id_bahan'];
+	$nama_bahan = $data['nama_bahan'];
+	$id_produk = $data['id_produk'];
+	$stok = $data['stok'];
+
+	$query = "UPDATE mahasiswa SET 
+			nama_bahan = '$nama_bahan', 
+			id_produk = '$id_produk',
+			stok = '$stok', 
+			WHERE id_bahan = $id_bahan
+			";
+	mysqli_query($conn, $query);
+	return  mysqli_affected_rows($conn);
+}
+
+function cari($key){
+	$query = "SELECT * FROM bahan WHERE nama LIKE '%$key%'";
+	return query($query);
+}	
--- CRUD bahan/tambah_bahan.php
+++ CRUD bahan/tambah_bahan.php
@@ -1,3 +1,28 @@
+<?php 
+require 'function.php';
+
+if (isset($_POST["submit"])) {
+
+	//cek data berhasil ditambah atau tidak
+	if(tambah_bahan($_POST) > 0){
+		echo "
+			<script>
+				alert('data berhasil ditambah');
+					document.location.href = 'index.php';
+			</script>
+		";
+	}else{
+		echo "
+			<script>
+				alert('data gagal ditambah'); 
+				document.location.href = 'index.php';
+			</script>
+		";
+	}
+}
+
+
+ ?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -11,7 +36,7 @@
 	<br/>
 	<br/>
 	<h3>TAMBAH BAHAN</h3>
-	<form method="post" action="tambah_bahan_aksi.php">
+	<form action="" method="POST" enctype="multipart/form-data">
 		<table>
 			<tr>			
 				<td>ID BAHAN</td>
