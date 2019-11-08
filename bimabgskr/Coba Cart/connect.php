68d6ad4fae88915c301830a069c51766a3c34287
bimabagaskara <55170641+bimabagaskara@users.noreply.github.com>
2019-11-08 18:09:50 +0700

    coba cart
    masih eror

---
+++ bimabgskr/Coba Cart/connect.php
@@ -0,0 +1,15 @@
+<?php 
+
+$host = "localhost";
+$user = "root" ;
+$password = "";
+$database = "product";
+
+$koneksi = mysqli_connect("$host","$user","$password","$database");
+
+// Check connection
+if ($koneksi -> connect_error) {
+	die("koneksi gagal : " . $koneksi -> connect_error);
+}
+echo"Mantap koneksi berhasil Sobat ERMAN";
+?>
