8aca632bdbc44ea5357d55e571dcda71c1d23e8e
FabryzalAdam <55338602+FabryzalAdam@users.noreply.github.com>
2019-12-10 12:38:02 +0700

    ubah

--- PrintOnline/frontend/uploaddesain.php
+++ PrintOnline/frontend/uploaddesain.php
@@ -1,75 +1,29 @@
 <?php
 session_start();
-include 'include/_header.php';
 require 'function.php';
+include 'include/_header.php';
 
-
-if (isset($_SESSION["LOGIN"])) {
-
-    $username = $_SESSION["LOGIN"];
-    $user = query("SELECT * FROM user where email = '$username'");
-
-    if (isset($_GET["cart"])) {
-
-        //cek data berhasil ditambah atau tidak
-        if (tambahcart($_GET) > 0) {
-            echo "
-			<script>
-				alert('data berhasil ditambah');
-				document.location.href = 'cart.php';
-			</script>
-		";
-        } else {
-            echo "
-			<script>
-				alert('data gagal ditambah'); 
-				
-			</script>
-		";
-        }
-    }
-} else {
-    echo "<script> alert('silahkan login terlebih dahulu!');</script>";
-    echo "<script>Location ='login.php'; </script>";
-}
-
-if (isset($_POST["submit"])) {
-    //cek data berhasil diubah atau tidak
-    if (uploaddesain($_POST) > 0) {
-        echo "
-			<script>
-				alert('data berhasil diubah');
-			</script>
-		";
-        header("Location:index.php");
-    } else {
-        echo "
-			<script>
-				alert('data gagal diubah'); 
-			</script>
-		";
-    }
-}
+$produk = query('SELECT * FROM produk order by rand()');
 
 ?>
-
+<!DOCTYPE html>
+<html>
 <!-- Main wrapper -->
 <div class="wrapper" id="wrapper">
-
     <!-- Header -->
     <?php include 'include/navbar.php'; ?>
     <!-- //Header -->
-    <!-- Start Search Popup -->
+    <!-- Start Bradcaump area -->
     <div class="ht__bradcaump__area bg-image--4">
         <div class="container">
             <div class="row">
                 <div class="col-lg-12">
                     <div class="bradcaump__inner text-center">
-                        <h2 class="bradcaump-title">upload desain</h2>
+                        <h2 class="bradcaump-title">Upload Design</h2>
                         <nav class="bradcaump-content">
                             <a class="breadcrumb_item" href="dashboard.php">Home</a>
                             <span class="brd-separetor">/</span>
-                            <span class="breadcrumb_item active">upload desain</span>
+                            <span class="breadcrumb_item active">Upload Design</span>
                         </nav>
                     </div>
                 </div>
@@ -77,24 +31,55 @@ if (isset($_POST["submit"])) {
         </div>
     </div>
 
-    <div class="form-group row">
-        <div class="col mb-3 mb-sm-0">
-            <label for="id_cart">nama Produk : </label>
-            <select class="form-control" name="id_cart" id="id_cart">
-                <option>Pilih Produk : </option>
-                <?php
-                $q = mysqli_query($conn, "SELECT * FROM keranjang WHERE username = '$username'");
-                while ($row = mysqli_fetch_array($q)) {
-                    $id_produk     = $row['id_produk'];
-                    $ba = mysqli_query($conn, "SELECT * FROM produk WHERE id_produk = '$id_produk'");
-                    $ro = mysqli_fetch_array($ba);
-                    echo "<option value=$row[id_produk]>$ro[jenis_produk]</option>";
-                } ?>
-            </select>
-        </div>
-    </div>
+    <head>
+        <title>The King | Upload Design</title>
+        <link rel="stylesheet" type="text/css" href="css.css">
+    </head>
+    <form>
 
-</div>
+        <body>
+            <div class="container col-md-5 mb-3">
+                <div class="head">
+                    <br><br>
+                </div>
+                <div class="kata col-md-4 mb-3">
+                    <p>Desain Anda</p>
+                </div>
+                <div class="image col-md-4 mb-3">
+                    <img src="no-image.png" id="gambar">
+                </div>
+                <p>Klik tombol pilih file untuk memilih file dari komputer anda.</p>
+                <div class="tombol">
+                    <input type="file" name="files" id="file" style="display: none;">
+                    <button id="pilih" class="btn-primary btn-lg">Pilih File</button>
+                    <button class="btn-success btn-lg">Upload Desain</button>
+                </div>
+            </div>
+    </form>
+    <script type="text/javascript">
+        var tm_pilih = document.getElementById('pilih');
+        var file = document.getElementById('file');
+        tm_pilih.addEventListener('click', function() {
+            file.click();
+        })
+        file.addEventListener('change', function() {
+            gambar(this);
+        })
+
+        function gambar(a) {
+            if (a.files && a.files[0]) {
+                var reader = new FileReader();
+                reader.onload = function(e) {
+                    document.getElementById('gambar').src = e.target.result;
+                }
+                reader.readAsDataURL(a.files[0]);
+            }
+
+        }
+    </script>
+    </body>
+
+</html>
 
 <?php
 include 'include/_footer.php';
