db64b533f1dcf176d97eab92df6f055a1c44201a
arfhdytllh <55170593+ariefhidayatullah@users.noreply.github.com>
2019-12-16 07:43:37 +0700

    ..

--- PrintOnline/admin/databahan/tambah.php
+++ PrintOnline/admin/databahan/tambah.php
@@ -21,6 +21,8 @@ if (isset($_POST["submit"])) {
 	}
 }
 
+include '../_header.php';
+
 $carikode = mysqli_query($conn, "SELECT id_bahan FROM bahan ") or die(mysqli_error($id_bahan));
 // menjadikannya array
 $datakode = mysqli_fetch_array($carikode);
@@ -43,209 +45,54 @@ if ($datakode) {
 
 
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<meta name="description" content="">
-	<meta name="author" content="">
-
-	<title>the king - tambah data</title>
-
-	<!-- Custom fonts for this template-->
-	<link href="../assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-	<link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-
-	<!-- Custom styles for this template-->
-	<link href="../assets/css/sb-admin-2.min.css" rel="stylesheet">
-
-</head>
-
-<body class="bg-gradient-primary">
-
-	<!-- Page Wrapper -->
-	<div id="wrapper">
-
-		<!-- Sidebar -->
-		<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
-
-			<!-- Sidebar - Brand -->
-			<a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-				<div class="sidebar-brand-icon rotate-n-15">
-					<i class="fas fa-laugh-wink"></i>
-				</div>
-				<div class="sidebar-brand-text mx-3">The King <sup>2</sup></div>
-			</a>
-
-			<!-- Divider -->
-			<hr class="sidebar-divider my-0">
-
-			<!-- Nav Item - Pages Collapse Menu -->
-			<!-- Nav Item - Dashboard -->
-			<li class="nav-item">
-				<a class="nav-link" href="dashboard/index.php">
-					<i class="fas fa-fw fa-tachometer-alt"></i>
-					<span>Dashboard</span></a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
-					<i class="fas fa-fw fa-folder"></i>
-					<span>Daftar Akun</span>
-				</a>
-				<div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
-					<div class="bg-white py-2 collapse-inner rounded">
-						<h6 class="collapse-header">login Admin / User</h6>
-						<a class="collapse-item" href="../auth/register.php">Tambah Akun Admin</a>
-						<a class="collapse-item" href="../auth/datakaryawan/datakaryawan.php">Data Karyawan</a>
-						<a class="collapse-item" href="../pengguna/datauser.php">List user / Pengguna</a>
-					</div>
-				</div>
-			</li>
-
-			<!-- Nav Item - Tables -->
-			<li class="nav-item">
-				<a class="nav-link" href="../dataproduk/dataproduk.php">
-					<i class="fas fa-fw fa-table"></i>
-					<span>Data Produk</span></a>
-			</li>
-
-			<!-- Nav Item - Tables -->
-			<li class="nav-item active">
-				<a class="nav-link" href="../databahan/databahan.php">
-					<i class="fas fa-fw fa-table"></i>
-					<span>Data Bahan</span></a>
-			</li>
-
-			<li class="nav-item">
-				<a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePagess" aria-expanded="true" aria-controls="collapsePages">
-					<i class="fas fa-fw fa-folder"></i>
-					<span>transaksi</span>
-				</a>
-				<div id="collapsePagess" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
-					<div class="bg-white py-2 collapse-inner rounded">
-						<h6 class="collapse-header">detail pemesanan</h6>
-						<a class="collapse-item" href="../transaksi/pembayaran/pembayaran.php">pembayaran</a>
-						<a class="collapse-item" href="../transaksi/pemesanan/pemesanan.php">pemesanan</a>
-					</div>
-				</div>
-			</li>
-
-			<li class="nav-item">
-				<a class="nav-link" href="../auth/logout.php">
-					<i class="fas fa-fw fa-power-off"></i>
-					<span>Logout</span></a>
-			</li>
-
-
-			<!-- Divider -->
-			<hr class="sidebar-divider d-none d-md-block">
-
-			<!-- Sidebar Toggler (Sidebar) -->
-			<div class="text-center d-none d-md-inline">
-				<button class="rounded-circle border-0" id="sidebarToggle"></button>
-			</div>
-
-		</ul>
-		<!-- End of Sidebar -->
-
-		<!-- Content Wrapper -->
-		<div id="content-wrapper" class="d-flex flex-column">
-
-			<!-- Main Content -->
-			<div id="content">
-
-				<!-- Topbar -->
-				<nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
-
-					<!-- Sidebar Toggle (Topbar) -->
-					<button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
-						<i class="fa fa-bars"></i>
-					</button>
-
-					<!-- Topbar Search -->
-					<form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
-						<div class="input-group">
-							<input type="text" class="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
-							<div class="input-group-append">
-								<button class="btn btn-primary" type="button">
-									<i class="fas fa-search fa-sm"></i>
-								</button>
-							</div>
-						</div>
-					</form>
-
-				</nav>
-				<!-- End of Topbar -->
-
-				<div class="container text-center">
-					<div class="text-center">
-						<h1 class="h4 text-gray-900 mb-4">Tambahkan Data!</h1>
-					</div>
-					<form class="user" method="post" action="">
-						<div class="form-group row">
-							<div class="col mb-3 mb-sm-0">
-								<input type="hidden" class="form-control form-control-static text-center" id="id_bahan" name="id_bahan" required value="<?= $kode_otomatis; ?>" readonly>
-							</div>
-						</div>
-						<div class="form-group row">
-							<div class="col mb-3 mb-sm-0">
-								<label for="nama_bahan">Nama Bahan :</label>
-								<input type="text" class="form-control form-control-static text-center" id="nama_bahan" name="nama_bahan" required placeholder="Masukkan Nama Bahan">
-							</div>
-						</div>
-						<div class="form-group row">
-							<div class="col mb-3 mb-sm-0">
-								<label for="id_produk">nama Produk : </label>
-								<select class="form-control" name="id_produk" id="id_produk">
-									<option>Pilih Produk : </option>
-									<?php
-									$q = mysqli_query($conn, "SELECT * FROM produk");
-									while ($row = mysqli_fetch_array($q)) {
-										echo "<option value=$row[id_produk]>$row[jenis_produk]</option>";
-									} ?>
-								</select>
-							</div>
-						</div>
-
-						<div class="form-group row">
-							<div class="col mb-3 mb-sm-0">
-								<label for="stok">Stok :</label>
-								<input type="text" class="form-control form-control-static text-center" id="stok" name="stok" required placeholder="Masukkan Jumlah Stok">
-							</div>
-						</div>
-						<div class="form-group row">
-							<div class="col mb-3 mb-sm-0">
-								<label for="harga_satuan">Harga Satuan :</label>
-								<input type="text" class="form-control form-control-static text-center" id="harga_satuan" name="harga_satuan" required placeholder="Masukkan Jumlah Harga">
-							</div>
-						</div>
-						<div class="pull-right text-center">
-							<a href="databahan.php" class="btn btn-warning">Kembali</a>
-							<input class="btn btn-primary" name="submit" type="submit" value="Tambahkan!"></input>
-						</div>
-					</form>
-				</div>
+<div class="container text-center">
+	<div class="text-center">
+		<h1 class="h4 text-gray-900 mb-4">Tambahkan Data!</h1>
+	</div>
+	<form class="user" method="post" action="">
+		<div class="form-group row">
+			<div class="col mb-3 mb-sm-0">
+				<input type="hidden" class="form-control form-control-static text-center" id="id_bahan" name="id_bahan" required value="<?= $kode_otomatis; ?>" readonly>
 			</div>
 		</div>
-	</div>
+		<div class="form-group ">
+			<div class="col-md-4 mb-3 mb-sm-0">
+				<label for="nama_bahan">Nama Bahan :</label>
+				<input type="text" class="form-control form-control-static text-center" id="nama_bahan" name="nama_bahan" required placeholder="Masukkan Nama Bahan">
+			</div>
+		</div>
+		<div class="form-group row">
+			<div class="col mb-3 mb-sm-0">
+				<label for="id_produk">nama Produk : </label>
+				<select class="form-control" name="id_produk" id="id_produk">
+					<option>Pilih Produk : </option>
+					<?php
+					$q = mysqli_query($conn, "SELECT * FROM produk");
+					while ($row = mysqli_fetch_array($q)) {
+						echo "<option value=$row[id_produk]>$row[jenis_produk]</option>";
+					} ?>
+				</select>
+			</div>
+		</div>
 
-	</div>
-
-	<!-- Bootstrap core JavaScript-->
-	<script src="../assets/vendor/jquery/jquery.min.js"></script>
-	<script src="../assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-	<!-- Core plugin JavaScript-->
-	<script src="../vassets/endor/jquery-easing/jquery.easing.min.js"></script>
-
-	<!-- Custom scripts for all pages-->
-	<script src="../assets/js/sb-admin-2.min.js"></script>
-
-</body>
-
-</html>
+		<div class="form-group row">
+			<div class="col mb-3 mb-sm-0">
+				<label for="stok">Stok :</label>
+				<input type="text" class="form-control form-control-static text-center" id="stok" name="stok" required placeholder="Masukkan Jumlah Stok">
+			</div>
+		</div>
+		<div class="form-group row">
+			<div class="col mb-3 mb-sm-0">
+				<label for="harga_satuan">Harga Satuan :</label>
+				<input type="text" class="form-control form-control-static text-center" id="harga_satuan" name="harga_satuan" required placeholder="Masukkan Jumlah Harga">
+			</div>
+		</div>
+		<div class="pull-right text-center">
+			<a href="databahan.php" class="btn btn-warning">Kembali</a>
+			<input class="btn btn-primary" name="submit" type="submit" value="Tambahkan!"></input>
+		</div>
+	</form>
+</div>
+<?php
+include '../_footer.php';
+?>
