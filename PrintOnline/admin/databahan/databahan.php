e646f9dfebb6608086a4137101cd62165cda9eb2
Fabryzal Adam Pramudya <ryzaldm@gmail.com>
2019-12-26 10:01:34 +0700

    modal tambah

--- PrintOnline/admin/databahan/databahan.php
+++ PrintOnline/admin/databahan/databahan.php
@@ -43,34 +43,6 @@ if ($datakode) {
 } else {
   $kode_otomatis = "B001";
 }
-
-
-?>
-
-<?php
-// var_dump($id);
-//query data mahasiswa berdasarkan ID
-
-$mhs = query("SELECT * FROM bahan WHERE id_bahan = '$id_bahan'");
-
-if (isset($_POST["submit"])) {
-    //cek data berhasil diubaahtau tidak
-    if (ubah($_POST) > 0) {
-        echo "
-      <script>
-        alert('data berhasil diubah');
-          document.location.href = 'databahan.php';
-      </script>
-    ";
-    } else {
-        echo "
-      <script>
-        alert('data gagal diubah'); 
-      </script>
-    ";
-    }
-}
-
 ?>
 <!-- End of Topbar -->
 
@@ -118,7 +90,8 @@ if (isset($_POST["submit"])) {
                       ?></td>
                 <td><?php echo $harga_satuan; ?></td>
                 <td>
-                  <button data-toggle="modal" data-id="<?= $id_bahan; ?>" data-target="#myModal" class="btn btn-warning btn-sm">Edit</button></a>
+                  <button hidden data-toggle="modal" data-id="<?= $id_bahan; ?>" data-target="#myModal" class="btn btn-warning btn-sm">Edit</button></a>
+                  <a href="ubah.php?id=<?= $data['id_bahan']; ?>" ><button class="btn btn-warning btn-sm">Ubah</button></a>
                   <a>||</a>
                   <a href="hapus.php?id=<?= $data['id_bahan']; ?>" onclick="return confirm('apakah anda yakin ? ');"><button class="btn btn-danger btn-sm">Hapus</button></a>
                 </td>
@@ -149,11 +122,9 @@ if (isset($_POST["submit"])) {
         <input type="hidden" class="form-control form-control-static text-center" id="id_bahan" name="id_bahan" required value="<?= $kode_otomatis; ?>" readonly>
       </div>
     </div>
-    <div class="form-row">
-      <div class="col">
         <label for="nama_bahan">Nama Bahan :</label>
         <input type="text" class="form-control form-control-static text-center" id="nama_bahan" name="nama_bahan" required placeholder="Masukkan Nama Bahan">
-      </div>
+      <div class="form-row">
       <div class="col">
         <label for="id_produk">nama Produk : </label>
         <select class="form-control" name="id_produk" id="id_produk">
@@ -165,8 +136,6 @@ if (isset($_POST["submit"])) {
           } ?>
         </select>
       </div>
-    </div>
-    <div class="form-group form-row">
       <div class="col">
         <label for="harga_satuan">Harga Satuan :</label>
         <input type="text" class="form-control form-control-static text-center" id="harga_satuan" name="harga_satuan" required placeholder="Masukkan Jumlah Harga">
@@ -182,65 +151,6 @@ if (isset($_POST["submit"])) {
   </div>
 </div>
 <!-- /.container-fluid -->
-
-<div class="modal fade" id="modaledit" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalCenterTitle">Ubah Data</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-      <div class="container text-center">
-    <?php foreach ($mhs as $rowo) : ?>
-
-        <form class="user" action="" method="POST" enctype="multipart/form-data">
-            <div class="form-group">
-                <div class="col mb-3 mb-sm-0">
-                    <input class="form-control form-control-static" type="hidden" name="id_bahan" id="id_bahan" required value="<?= $rowo['id_bahan']; ?>" readonly>
-                </div>
-            </div>
-            <div class="form-group">
-                <div class="col mb-3 mb-sm-0">
-                    <label for="nama_bahan">Nama Bahan : </label>
-                    <input class="form-control form-control-static" type="text" name="nama_bahan" id="nama_bahan" required value="<?= $rowo['nama_bahan']; ?>">
-                </div>
-            </div>
-            <div class="form-group">
-                <div class="col mb-3 mb-sm-0">
-                    <label for="harga_satuan">Harga Satuan : </label>
-                    <input class="form-control form-control-static" type="text" name="harga_satuan" id="harga_satuan" required value="<?= $rowo['harga_satuan']; ?>">
-                </div>
-            </div>
-            <div class="form-group">
-                <div class="col mb-3 mb-sm-0">
-                    <label for="id_produk">id produk : </label>
-                    <select class="form-control" name="id_produk" id="id_produk">
-                        <option>pilih produk : </option>
-                        <?php
-                            $q = mysqli_query($conn, "SELECT * FROM produk");
-                            while ($row = mysqli_fetch_array($q)) {
-                                echo "<option value=$rowo[id_produk]>$rowo[jenis_produk]</option>";
-                            } ?>
-                    </select>
-                </div>
-            </div>
-            <a href="databahan.php" class="btn btn-warning">
-                Kembali
-            </a>
-    <?php endforeach; ?>
-</div>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button class="btn btn-primary" name="submit" type="submit">Ubah</button>
-        </form>
-      </div>
-    </div>
-  </div>
-</div>
 
 <?php
 include '../_footer.php';
--- PrintOnline/admin/dataproduk/dataproduk.php
+++ PrintOnline/admin/dataproduk/dataproduk.php
@@ -2,6 +2,26 @@
 require 'function.php';
 $bahan = query('SELECT * FROM produk');
 include '../_header.php';
+
+if (isset($_POST["submit"])) {
+
+  //cek data berhasil ditambah atau tidak
+  if (tambah($_POST) > 0) {
+    echo "
+      <script>
+        alert('data berhasil ditambah');
+          document.location.href = 'dataproduk.php';
+      </script>
+    ";
+  } else {
+    echo "
+      <script>
+        alert('data gagal ditambah'); 
+        document.location.href = 'dataproduk.php';
+      </script>
+    ";
+  }
+}
 ?>
 
 <!-- Begin Page Content -->
@@ -14,6 +34,7 @@ include '../_header.php';
     <div class="card-body">
       <div class="table-responsive">
         <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0" id="dokter">
+          <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">Tambah Bahan</button>
           <thead>
             <tr>
               <th>No.</th>
@@ -44,12 +65,55 @@ include '../_header.php';
             <?php endforeach; ?>
           </tbody>
         </table>
-        <a href="tambah.php" class="btn btn-primary" role="button"> Tambah data </a>
+        <a href="tambah.php" class="btn btn-primary" role="button"> Tambah Data </a>
       </div>
     </div>
   </div>
 </div>
 <!-- /.container-fluid -->
+
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="card-title">Tambahkan data produk</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+    <form class="user" method="post" action="" enctype="multipart/form-data">
+    <div class="form-group row">
+      <div class="col mb-3 mb-sm-0">
+        <input type="hidden" class="form-control form-control-static text-center" id="id_produk" name="id_produk" value="" readonly>
+      </div>
+    </div>
+    <div class="form-group row">
+      <div class="col mb-3 mb-sm-0">
+        <label for="jenis_produk">Masukkan Jenis Produk : </label>
+        <input type="text" class="form-control form-control-static text-center" id="jenis_produk" name="jenis_produk" required placeholder="Masukkan jenis produk">
+      </div>
+
+
+      <div class="col mb-3 mb-sm-0">
+        <label for="deskripsi">Tambahkan Deskripsi : </label>
+        <input type="text" class="form-control form-control-static text-center" id="deskripsi" name="deskripsi" required placeholder="Tambahkan Deskripsi">
+      </div>
+      <div class="col">
+        <input type="file" id="gambar" name="gambar">
+      </div>
+    </div>
+    <div class="pull-right text-center">
+      </div>
+</div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <input class="btn btn-primary ml-5" name="submit" type="submit" value="Tambahkan"></input>
+      </div>
+      </form>
+    </div>
+  </div>
+</div>
 
 <?php
 include '../_footer.php';
